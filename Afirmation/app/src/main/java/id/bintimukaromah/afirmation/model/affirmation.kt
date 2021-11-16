16764f119c0a89fddaf9b5b1ad8841ce156753a7
Arum18 <195410016@students.akakom.ac.id>
2021-11-16 10:55:23 +0700

    Afirmasi

---
+++ Afirmation/app/src/main/java/id/bintimukaromah/afirmation/model/affirmation.kt
@@ -0,0 +1,9 @@
+package id.bintimukaromah.afirmation.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int) {
+}
