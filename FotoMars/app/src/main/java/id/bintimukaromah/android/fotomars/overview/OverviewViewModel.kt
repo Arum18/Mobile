0ee6c3b9e9f657e8e704cf00d56326abb0db951a
Arum18 <195410016@students.akakom.ac.id>
2021-12-10 16:41:14 +0700

    ForoMars

---
+++ FotoMars/app/src/main/java/id/bintimukaromah/android/fotomars/overview/OverviewViewModel.kt
@@ -0,0 +1,23 @@
+package id.bintimukaromah.android.fotomars.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<String>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<String> = _status
+
+    init {
+        getMarsPhotos()
+    }
+
+    private fun getMarsPhotos() {
+        _status.value = "Set the Mars API status response here!"
+    }
+}
